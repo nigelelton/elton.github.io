a03bdbe4757a0ebfc8274fa999657762b02f0a18
nigelelton <nigelelton@Nigels-MacBook-Pro.local>
2014-11-18 08:08:34 -0500

    Fixed JS
    My javascript broke so I reverted to an older version.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
+
 var $win = $(window);
 var $heroSection = $('.hero-section');
 
 $win.on('scroll', function () {
-	var scrollPos = $win.scrollTop();
+  var scrollPos = $win.scrollTop();
 
-	$heroSection.css('background-position', 'center ' + scrollPos /2.5 + 'px');
+  $heroSection.css('background-position', 'center ' + scrollPos /2.5 + 'px');
 });
 
 var fixed_nav_start = $('#about').position().top ;
