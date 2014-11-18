129e95512a86664d79611b72dfc149d5d3ff88f9
nigelelton <nigelelton@Nigels-MacBook-Pro.local>
2014-11-18 05:47:53 -0500

    Major overhaul to html, css and js
    It was a long night. I’m tired. Lots of work was accomplished. Massive
    overhaul to everything.

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ $win.on('scroll', function () {
 
 	$heroSection.css('background-position', 'center ' + scrollPos /2.5 + 'px');
 });
+
+var fixed_nav_start = $('#about').position().top ;
+$win.scroll(function () {
+    if( $(this).scrollTop() > fixed_nav_start ){
+        $('.hero-section nav').addClass('fixed-nav');
+    }else{
+        $('.hero-section nav').removeClass('fixed-nav');
+    }
+});
