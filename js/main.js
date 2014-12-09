c7ad9228e22a9888557be8f51fe2cffebc45ecad
nigelelton <nigelelton@Nigels-MacBook-Pro.local>
2014-12-08 22:40:45 -0500

    MAJOR OVERHAUL
    Added a lightbox plugin for my gallery images. Also added a favicon for
    both my main and 404 page. Created a fix for a bug happening in my
    navigation where the script couldn’t tell when to pop the background in.

--- js/main.js
+++ js/main.js
@@ -3,16 +3,20 @@ var $win = $(window);
 var $heroSection = $('.hero-section');
 
 $win.on('scroll', function () {
-  var scrollPos = $win.scrollTop();
+	var scrollPos = $win.scrollTop();
 
-  $heroSection.css('background-position', 'center ' + scrollPos /2.5 + 'px');
+	$heroSection.css('background-position', 'center ' + scrollPos /2.5 + 'px');
 });
 
-var fixed_nav_start = $('#about').position().top ;
-$win.scroll(function () {
-    if( $(this).scrollTop() > fixed_nav_start ){
-        $('.hero-section nav').addClass('fixed-nav');
-    }else{
-        $('.hero-section nav').removeClass('fixed-nav');
-    }
-});
+if( $('.nav-fixed').length ) {
+	var fixed_nav_start = $('.nav-fixed').position().top - 100;
+
+	$win.scroll(function () {
+	    if( $(this).scrollTop() > fixed_nav_start ){
+	        $('.hero-section nav').addClass('fixed-nav');
+	    }else{
+	        $('.hero-section nav').removeClass('fixed-nav');
+	    }
+	});
+
+}
